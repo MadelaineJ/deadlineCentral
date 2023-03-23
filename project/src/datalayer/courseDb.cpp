e9ac0d9d905710c659d8269a3824315013ec8dbf
Kaylehb Edward Trotter States <stateske@stumail.csci.viu.ca>
2023-03-23 15:55:58 -0700

    wrote stubs for courseDb.cpp

--- project/src/datalayer/courseDb.cpp
+++ project/src/datalayer/courseDb.cpp
@@ -1,1 +1,29 @@
 #include "courseDb.hpp"
+
+bool createCourse(Course C){
+   return 0;
+}
+
+bool updateCourse(Course C){
+   return 0;
+}
+
+bool deleteCourse(Course C){
+   return 0;
+}
+
+list<Student> getEnrollmentList(int courseID){
+   list<Student> L;
+   return L;
+}
+
+Course getCourseInfo(int courseID){
+   list<int> L;
+   Course C(0,0,0,L);
+   return C;
+}
+
+//TODO return type tbd
+void aggregateDeadlines(int courseID){
+
+}
