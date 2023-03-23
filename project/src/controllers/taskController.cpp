94f07bcd3f7cbbfa021de95cadea6d43df535b8c
Davon Taylor <taylord@stumail.csci.viu.ca>
2023-03-23 15:30:33 -0700

    added stub functions to taskController cpp

--- project/src/controllers/taskController.cpp
+++ project/src/controllers/taskController.cpp
@@ -1,1 +1,41 @@
+// Required Libraries
+#include <iostream>
+#include <string>
+#include <termios.h>
+#include <unistd.h>
+
+
+// Custom header files
 #include "taskController.hpp"
+
+using namespace std;
+
+// constructor
+TaskController::TaskController()
+{
+
+}
+
+// destructor
+TaskController::~TaskController() { }
+
+// createTask
+void TaskController::createTask() {
+	
+}
+
+// addTask
+void TaskController::addTask() {
+    
+}
+
+// editTask
+void TaskController::editTask() {
+
+}
+
+// getTaskInfo
+void TaskController::getTaskInfo() {
+
+}
+
