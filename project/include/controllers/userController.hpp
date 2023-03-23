8fb8fb8b719ecf68689c57bef96d6a016e1c74fb
Kaylehb Edward Trotter States <stateske@stumail.csci.viu.ca>
2023-03-22 20:50:26 -0700

    wrote userController.hpp

---
+++ project/include/controllers/userController.hpp
@@ -0,0 +1,27 @@
+#ifndef USERCONTROLLER_H
+#define USERCONTROLLER_H
+
+// Required Libraries
+#include <iostream>
+#include <string>
+#include <termios.h>
+#include <unistd.h>
+using namespace std;
+
+
+class UserController 
+{
+    public:
+        UserController(){}
+        ~UserController(){}
+        
+        void createStudent();
+
+        void createInstructor();
+
+        void deleteUser();
+
+        void getUserInfo();
+};
+
+#endif
