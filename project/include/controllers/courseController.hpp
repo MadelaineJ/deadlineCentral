474eb763b2f2d1bde97b80c2c1dc442098391f7a
Kaylehb Edward Trotter States <stateske@stumail.csci.viu.ca>
2023-03-22 20:25:54 -0700

    Wrote courseController.hpp

---
+++ project/include/controllers/courseController.hpp
@@ -0,0 +1,33 @@
+#ifndef COURSECONTROLLER_H
+#define COURSECONTROLLER_H
+
+// Required Libraries
+#include <iostream>
+#include <string>
+#include <termios.h>
+#include <unistd.h>
+using namespace std;
+
+
+class CourseController 
+{
+    public:
+        CourseController(){}
+        ~CourseController(){}
+        
+        void createCourse();
+
+        void editCourse();
+
+        void deleteCourse();
+
+        void getCourseInfo();
+
+        void addTask();
+
+        void removeTask();
+
+        void aggregateDeadlines();
+};
+
+#endif
