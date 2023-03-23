2291b2ca0b9dcbf7d7159102c4e9b2398ad2939f
Brandon Cosh <coshb@stumail.csci.viu.ca>
2023-03-22 19:49:17 -0700

    added stubs for subscriptionDB in the both hpp and cpp files

---
+++ project/include/datalayer/subscriptionDb.hpp
@@ -0,0 +1,32 @@
+#ifndef CONTROLLER_DB_H
+#define CONTROLLER_DB_H
+
+// Required Libraries
+#include <iostream>
+#include "occi.h"  // Oracle's C++ call level interface library package
+#include <termios.h>
+#include <unistd.h>
+#include <list>
+#include <string>
+
+#include "../models/course.hpp"
+
+using namespace std;
+using namespace oracle::occi;
+
+class SubscriptionDB 
+{
+    public:
+        SubscriptionDB();
+        ~SubscriptionDB();
+
+        Course* getAvailableCourseList(int courseID);
+        Course* getCourseSubscriptions(int studentID);
+        bool deleteSubscription(int studentID, int courseID);
+        bool createSubscription(int studentID, int courseID);
+    private:
+        // helper functions
+        int getCourseID(std::string courseCode, std::string instructorName);
+};
+
+#endif // CONTROLLER_DB_H
--- project/src/datalayer/subscriptionDb.cpp
+++ project/src/datalayer/subscriptionDb.cpp
@@ -1,1 +1,36 @@
 #include "subscriptionDb.hpp"
+
+
+SubscriptionDB::SubscriptionDB(){
+
+}
+
+SubscriptionDB::~SubscriptionDB(){
+
+}
+
+Course* SubscriptionDB::getAvailableCourseList(int courseID) {
+    // query DB table Courses to return list of all courses
+    return NULL;
+}
+
+Course* SubscriptionDB::getCourseSubscriptions(int studentID) {
+    // query DB table StudentCourses using studentID to get all courses subscribed to
+    return NULL;
+}
+
+bool SubscriptionDB::deleteSubscription(int studentID, int courseID) {
+    // query DB table StudentCourses to remove subscription
+    return false;
+}
+
+bool SubscriptionDB::createSubscription(int studentID, int courseID) {
+    // query DB table StudentCourses to create subscription
+    return false;
+}
+
+// helper functions
+int SubscriptionDB::getCourseID(std::string courseCode, std::string instructorName) {
+    // query DB table Courses using courseCode & instructor name to find courseID
+    return false;
+}
