bcec224ee79612e2b5a567b3e75e465825a8121e
Kaylehb Edward Trotter States <stateske@stumail.csci.viu.ca>
2023-03-22 19:15:15 -0700

    Wrote courseDb.hpp

---
+++ project/include/datalayer/courseDb.hpp
@@ -0,0 +1,32 @@
+#ifndef COURSE_DB_H
+#define COURSE_DB_H
+
+// Required Libraries
+#include <iostream>
+#include "occi.h"  // Oracle's C++ call level interface library package
+#include <termios.h>
+#include <unistd.h>
+#include "../models/course.hpp"
+#include "../models/student.hpp"
+
+using namespace std;
+using namespace oracle::occi;
+
+class CourseDB 
+{
+    public:
+        CourseDB(){}
+        ~CourseDB(){}
+        bool createCourse(Course C);
+        bool updateCourse(Course C);
+        bool deleteCourse(Course C);
+        //TODO change return type to list<Student> when implemented
+        void getEnrollmentList(int courseID);
+        //TODO change return type to Course when implemented
+        void getCourseInfo(int courseID);
+        //return type tbd
+        void aggregateDeadlines(int courseID);
+    private:
+};
+
+#endif 
