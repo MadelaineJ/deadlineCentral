373f754978c01ceff80fc94ddbd0618ed1d53b19
Madelaine Eva Marie Jones <jonesme@stumail.csci.viu.ca>
2023-04-06 10:07:55 -0700

    removed unused files and databaseTest program

--- project/include/databaseTest.hpp
+++
@@ -1,7 +0,0 @@
-// Test main program to access oracle
-#include <iostream>
-#include <occi.h>  // Oracle's C++ call level interface library package
-#include <termios.h>
-#include <unistd.h>
-
-void helloWorldDb();
--- project/include/sqrt.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef SQRT_H
-#define SQRT_H
-
-// Get the Square root of a number. 
-double squareRoot(const double a);
-
-#endif
